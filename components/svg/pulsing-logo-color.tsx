0304af9eb5318084a8e6532210347fa7a98ab4ea
Gaz <gaz@Gazs-MacBook-Pro.local>
2024-11-30 23:49:25 +0000

    udpate viewbox of pulsing logo color so that it appears centred in page properly.

--- components/svg/pulsing-logo-color.tsx
+++ components/svg/pulsing-logo-color.tsx
@@ -12,7 +12,7 @@ interface PulsingLogoColorProps extends MotionSVGProps {
 const PulsingLogoColor = (props: PulsingLogoColorProps) => (
   <motion.svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 1000 512"
+    viewBox="0 0 850 500"
     id="Layer_1"
     data-name="Layer 1"
     {...props}
