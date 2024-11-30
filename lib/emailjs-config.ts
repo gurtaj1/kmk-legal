f1b03fa13ae6765872ccb6373269fcfd15560c33
Gaz <gaz@Gazs-MacBook-Pro.local>
2024-11-30 00:02:41 +0000

    update emailjs config.

--- lib/emailjs-config.ts
+++ lib/emailjs-config.ts
@@ -1,5 +1,5 @@
 export const EMAILJS_CONFIG = {
-  SERVICE_ID: "your_service_id",
-  TEMPLATE_ID: "template_sdemv28",
+  SERVICE_ID: "service_rah495h",
+  TEMPLATE_ID: "template_9mh72oi",
   PUBLIC_KEY: "n0f7HYec1zFa8Eram",
 } as const;
