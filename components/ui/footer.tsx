c262acf5ae984d768c99644246bacc3c1f98def7
Gaz <gaz@Gazs-MacBook-Pro.local>
2024-12-01 09:10:28 +0000

    add links to social media profiles.

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -15,6 +15,17 @@ import {
 
 import { kmkColors } from "@/constants";
 
+const TikTokIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className={className}
+  >
+    <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z" />
+  </svg>
+);
+
 const Footer = () => {
   return (
     <footer className="bg-kmk-logoBlue text-white p-8">
@@ -26,17 +37,35 @@ const Footer = () => {
             mFill={kmkColors.logoGrey}
           />
           <div className="flex gap-4 mt-5">
-            <Link href="#" className="text-white hover:text-[#FFD700]">
+            <Link
+              href="https://www.facebook.com/share/1KzcDWawZk/"
+              className="text-white hover:text-[#FFD700]"
+            >
               <Facebook className="h-5 w-5" />
             </Link>
-            <Link href="#" className="text-white hover:text-[#FFD700]">
+            <Link
+              href="https://x.com/KmkLegal86577?t=indWvXOr4yU86Xg1v9sdEA&s=08"
+              className="text-white hover:text-[#FFD700]"
+            >
               <Twitter className="h-5 w-5" />
             </Link>
-            <Link href="#" className="text-white hover:text-[#FFD700]">
+            <Link
+              href="https://www.instagram.com/kmk.legal/"
+              className="text-white hover:text-[#FFD700]"
+            >
               <Instagram className="h-5 w-5" />
             </Link>
-            <Link href="#" className="text-white hover:text-[#FFD700]">
+            <Link
+              href="https://www.linkedin.com/company/kmk-legal/"
+              className="text-white hover:text-[#FFD700]"
+            >
               <Linkedin className="h-5 w-5" />
+            </Link>
+            <Link
+              href="https://www.tiktok.com/@infowad5pv?_t=ZG-8rq9mCzXm6b&_r=1"
+              className="text-white hover:text-[#FFD700]"
+            >
+              <TikTokIcon className="h-5 w-5" />
             </Link>
           </div>
           <p className="mt-4">
